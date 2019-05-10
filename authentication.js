c4cdfaf81055c851f64ae91f073be6aa181c433e
derfloh205 <derfloh205@gmail.com>
2019-05-10 13:04:47 +0200

    added register

--- authentication.js
+++ authentication.js
@@ -20,9 +20,11 @@ function login() {
                 // success, redirect
                 window.open("universities.html");
             } else {
+                alert("Login Error");
                 // show login error
             }
         } else {
+            alert("Connection Error");
             // show connection error
         }
     };
@@ -33,6 +35,30 @@ function login() {
 
 function register() {
 
+    // get student-id and pw from form
+    let studentID = document.getElementById("studentNumber").value;
+    let studentName = document.getElementById("studentName").value;
+    let studentEmail = document.getElementById("studentEmail").value;
+    let studentPW = document.getElementById("studentPW").value;
 
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = this.response;
+            if(response) {
+                // success, redirect
+                window.open("universities.html");
+            } else {
+                alert("Registration Error");
+                // show register error
+            }
+        } else {
+            alert("Connection Error");
+            // show connection error
+        }
+    };
+    xmlhttp.open("POST", "register.php", true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send("id=" + studentID + "&name=" + studentName + "&email=" + studentEmail + "&pw=" + studentPW);
 }
 
