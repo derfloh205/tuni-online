dc531484bb30094809e974bc517947adefecbdea
Anja Reibenbacher <anja.reibenbacher@student.tugraz.at>
2019-05-10 15:24:24 +0200

    add products and lectures row in array

--- lecturesAndProductsFromLectures.php
+++ lecturesAndProductsFromLectures.php
@@ -17,10 +17,13 @@ $query = "SELECT * FROM universityproducts";
 $universityproducts = mysqli_query($db, $query) or die('Error querying database 1.');
 
 $rows = array();
+array_push($row, "lectures");
 while($element = mysql_fetch_assoc($lectures))
 {
 	$rows[] = $element;
 }
+
+array_push($row, "products");
 
 while($element = mysql_fetch_assoc($universityproducts))
 {
