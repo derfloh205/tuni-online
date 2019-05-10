a4344f41d29f22c947f434370eeb13dd5b5de1bf
Alexander Grass <alexanderwaltergrass@gmail.com>
2019-05-10 15:44:18 +0200

    added getUniversities

--- getUniversities.php
+++ getUniversities.php
@@ -4,10 +4,22 @@
     $password = "";
     $db = "tunidb";
 
-
     // Create connection
     $conn = new mysqli($servername, $username, $password, $db);
     $sql = "SELECT name FROM universities";
     $result = $conn->query($sql);
-    echo json_encode($result);
+    if($result->num_rows == 0)
+    {
+        echo false
+    }
+    else
+    {
+        $rows = array();
+        while($r = mysqli_fetch_assoc($result)) {
+            $rows[] = $r;
+        }
+        echo json_encode($rows);
+    }
+    $result->close();
+    $conn->close();
 ?>
