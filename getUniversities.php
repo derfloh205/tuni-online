0e7eb59329fe58a68915b7c02377e7768c469d61
Simon Guggi <simonguggi@tug-swl-228-215.tugraz.at>
2019-05-10 15:53:31 +0200

    fixed getUniversities

--- getUniversities.php
+++ getUniversities.php
@@ -6,7 +6,7 @@
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $db);
-    $sql = "SELECT name FROM universities";
+    $sql = "SELECT name FROM university";
     $result = $conn->query($sql);
     if($result->num_rows == 0)
     {
@@ -20,6 +20,5 @@
         }
         echo json_encode($rows);
     }
-    $result->close();
     $conn->close();
 ?>
