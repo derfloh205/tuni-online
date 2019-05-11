226a4c8c96b6b07c747b40104345714d648abb94
tinabambina <tina.promitzer@student.tugraz.at>
2019-05-11 15:31:40 +0200

    adaptions

--- getLecturesAndProductsFromUniversity.php
+++ getLecturesAndProductsFromUniversity.php
@@ -1,35 +1,38 @@
 <?php
+if (array_key_exists('id', $_POST)) {
+    $university = $_POST['id'];
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "tunidb";
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbName = "tunidb";
 
-$university = (isset($_POST['id']) ? $_POST['id'] : '');
+		$conn = new mysqli($servername, $username, $password, $dbName);
 
-$conn = new mysqli($servername, $username, $password, $dbName);
+		$query_string = "select lectureID from universitylectures where uniID = '$university'";
+		$lectures = $conn->query($query_string) or die('Error connecting to MySQL server.');
 
-$query_string = "select lectureID from universitylectures where uniID = " . $university;
-$lectures = $conn->query($query_string) or die('Error connecting to MySQL server.');
+		$rows[] = "lectures";
+		while($element = mysqli_fetch_assoc($lectures))
+		{
+			$query_string = "select * from lecture where ID = '$element'";
+			$lecture_name = $conn->query($query_string) or die('Error connecting to MySQL server.');
+			$rows[] = $lecture_name;
+		}
 
-$rows[] = "lectures";
-while($element = mysqli_fetch_assoc($lectures))
-{
-	$query_string = "select * from lecture where ID = " . $element;
-	$lecture_name = $conn->query($query_string) or die('Error connecting to MySQL server.');
-	$rows[] = $lecture_name;
+		$product_query = "select productID from universityproducts where uniID = '$university'";
+		$products = $conn->query($product_query) or die('Error connecting to MySQL server.');
+		$rows[] = "products";
+
+		while($element = mysqli_fetch_assoc($products))
+		{
+			$query_string = "select * from product where ID = '$element'";
+			$procuct = $conn->query($query_string);
+			$rows[] = $procuct;
+		}
+
+
+		echo json_encode($rows);
 }
 
-$product_query = "select productID from universityproducts where uniID = (". $university . ")"
-$products = $conn->query($product_query) or die('Error connecting to MySQL server.');
-$rows[] = "products";
-
-while($element = mysqli_fetch_assoc($products))
-{
-	$query_string = "select * from product where ID = " . $element;
-	$procuct = $conn->query($query_string);
-	$rows[] = $procuct;
-}
-
-echo json_encode($rows);
 ?>
